e65b9fd7e8c68f4476799cdece1d40e9418fcb66
Valeria Curseri <curserivaleria@gmail.com>
2020-12-12 14:03:45 +0100

    api con domande, risposte, e conteggio domande con next()

--- quiz/src/App.vue
+++ quiz/src/App.vue
@@ -4,7 +4,13 @@
     <b-container class="bv-example-row">
       <b-row>
         <b-col sm="6" offset-sm="3">
-          <QuestionBox />
+          <!-- uso v-bind e attributi personalizzati per passare delle variabili al template. passo nella variabile currentQuestion l'istanza index dell'array questions, e nella variabile next la fx next -->
+          <!-- v-if aggiunto perchè altrimenti funzionava ma :currentQuestion dava undefined. così ritardiamo il caricamento del box a quando l'array questions è pieno -->
+          <QuestionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]" 
+            :next="next"
+          />
         </b-col>
       </b-row>
     </b-container>
@@ -21,12 +27,26 @@ export default {
     Header,
     QuestionBox
   },
+  data() {              // invece di creare un'oggetto 'data' ritorno un oggetto 'data' in una funzione ES6 - necessario quando abbiamo una single page application in cui componenti richiamano dati diversi
+    return {
+      questions: [],    // creo un array vuoto dove andrò a inserire i dati in formato json
+      index: 0          // index delle domande, come counter
+    }
+  },
+  methods: {
+    next(){             // incremento l'indice della domanda di 1
+      this.index++
+    }
+  },
   mounted: function(){                                                          // quando App è stato creato
     fetch('https://opentdb.com/api.php?amount=10&category=9&type=multiple', {   // faccio una chiamata all'api
       method: 'get'                                                             // prendo i dati
     })
-      .then((response) => {                                                     // li stampo
-        console.log(response.json())
+      .then((response) => {                                                     
+        return response.json()                                                  // li ottengo in formato json
+      })
+      .then((jsonData) => {
+        this.questions = jsonData.results                                       // inserisco i dati in formato json nell'array vuoto all'interno della fz data()
       })
   }
 }
--- quiz/src/components/QuestionBox.vue
+++ quiz/src/components/QuestionBox.vue
@@ -2,17 +2,38 @@
     <div class="question-box-container">
         <b-jumbotron>
             <template #lead>
-                Some questione here?
+                {{ currentQuestion.question }} <!-- stampo la domanda contenuta nell'oggetto passato tramite la variabile currentQuestion da App.vue -->
             </template>
             
             <hr class="my-4">
             
-            <p>
-                List of answers
-            </p>
+            <ul>
+                <!-- ciclo tutto l'array answers e stampo le risposte -->
+                <li v-for="(answer, index) in answers" :key="index">{{ answer }}</li>
+            </ul>
             
             <b-button variant="primary" href="#">Submit</b-button>
-            <b-button variant="success" href="#">Next</b-button>
+            <b-button @click="next" variant="success" href="#">    <!-- al click sul btn next attivo la fz next -->
+                Next
+            </b-button>
         </b-jumbotron>
     </div>
 </template>
+
+<script>
+    // per fare in modo che l'oggetto che passo nella variabile currentQuestion venga stampato in {{}} serve un passaggio nello script e nei props
+    export default {
+        props: {
+            currentQuestion: Object,
+            next: Function
+        },
+        computed: {
+            answers() {                                                     // memorizzo nella cache - una volta
+                let answers = [...this.currentQuestion.incorrect_answers]   // inserisco nell'array answers tutte le risposte sbagliate
+                answers.push(this.currentQuestion.correct_answers)          // inserisco nell'array answers la risposta corretta
+                return answers
+            }
+        }
+    }
+
+</script>
