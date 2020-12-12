df238d8e7dee0875a8f3b7dc15fac181eac76c0f
Valeria Curseri <curserivaleria@gmail.com>
2020-12-12 16:26:34 +0100

    condizioni per i colori di sfondo

--- quiz/src/components/QuestionBox.vue
+++ quiz/src/components/QuestionBox.vue
@@ -9,23 +9,24 @@
             
             <!-- ciclo tutto l'array answers e stampo le risposte -->
             <!-- al click su una risposta memorizzo l'index e lo passo alla fz selectAnswer, che la salverà tra i data del component -->
+            <!-- invece di mettere in :class tutte le condizioni per determinare il colore di sfondo della casella, le inserisco in un metodo che richiamo in :class -->
             <b-list-group>
                 <b-list-group-item 
                     v-for="(answer, index) in shuffledAnswers" 
                     :key="index"
                     @click="selectAnswer(index)" 
-                    :class="[selectedIndex === index ? 'selected' : '']"
+                    :class="answerClass(index)"
                 >
                     {{ answer }}
                 </b-list-group-item>
             </b-list-group>
             
             <!-- al click sul btn next attivo la fz submitAnswer che verifica se la risposta è corretta o meno -->
-            <!-- il btn submit è disattivato se non ho selezionato nessuna risposta -->
+            <!-- il btn submit è disattivato se non ho selezionato nessuna risposta e se ho già cliccato submit-->
             <b-button 
                 @click="submitAnswer" 
                 variant="primary" 
-                :disable="selectedIndex === null"
+                :disabled="selectedIndex === null || answered"
             >
             Submit
             </b-button>
@@ -53,7 +54,8 @@
             return {
                 selectedIndex: null,        // imposto selectedIndex a null e lo sovrascrivo al click con la fz selectAnswer
                 correctIndex: null,         // imposto correctIndex a null e lo sovrascrivo al click con la fz shuffleAnswers
-                shuffledAnswers: []         // imposto un array vuoto in cui andrò a pushare le risposte in ordine casuale
+                shuffledAnswers: [],        // imposto un array vuoto in cui andrò a pushare le risposte in ordine casuale
+                answered: false             // imposto la variabile answered a false. diventerà true quando una risposta verrà selezionata, nella fz submitAnswer
             }
         },
         computed: {
@@ -68,6 +70,7 @@
                 immediate:true,                 // partirà subito
                 handler() {                     // svolgendo queste fz. anche quando la currentQuestion nei props cambierà
                     this.selectedIndex = null   // imposto selectedIndex nuovamente a null così potrò sovrascriverlo di nuovo al click con la fz selectAnswer
+                    this.answered = false       // al next, answered rimarrà false
                     this.shuffleAnswers()       // avvio la fz shuffleAnswers() che tramite shuffle riempie l'array shuffledAnswers[] con le answers in ordine casuale
                 }
             }
@@ -79,14 +82,31 @@
             shuffleAnswers() {
                 let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
                 this.shuffledAnswers = _.shuffle(answers)   // _ appartiene alla sintassi di shuffle e l'ho importato all'inizio dello script
-                this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+                this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer) // restituisce l'index della risposta corretta (this.currentQuestion.correct_answer) all'interno dell'array in cui le risposte sono in ordine casuale (this.shuffledAnswers)
             },
             submitAnswer(){
                 let isCorrect = false                           // imposto una variabile sentinella a false
                 if (this.selectedIndex === this.correctIndex) { // se l'index della risposta selezionata corrisponde all'index della risposta corretta
                     isCorrect = true                            // la variabile sentinella diventa true
                 }
+                this.answered = true
                 this.increment(isCorrect)                       // se la variabile sentinella è true, parte la fz increment (settata tra i props, si trova in App.vue e stampa su Header.vue )
+            },
+            answerClass(index){
+                let answerClass = ""
+                if (!this.answered && this.selectedIndex === index) {
+                    answerClass = 'selected'
+                } else if (this.answered && this.correctIndex === index) {
+                    answerClass = 'correct'
+                } else if (this.answered && this.selectedIndex === index && this.selectedIndex !== this.correctIndex) {
+                    answerClass = 'incorrect'
+                }
+                return answerClass
+                //  [
+                //      !answered && selectedIndex === index ? 'selected' : '',
+                //      answered && correctIndex === index ? 'correct' : '',
+                //      answered && selectedIndex === index && selectedIndex !== correctIndex ? 'incorrect' : ''
+                //  ]
             }
         }
     }
@@ -105,10 +125,12 @@
         background-color: lightblue!important;
     }
     .correct{
-        background-color: green;
+        background-color: green!important;
+        color:#fff;
     }
     .incorrect{
-        background-color: red;
+        background-color: red!important;
+        color:#fff;
     }
     .btn{
         margin:0 5px;
